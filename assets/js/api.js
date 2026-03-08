4d46d79880b45ad9bf157d96c4864a34ebaa2833
aeeayemen <129388062+aeeayemen@users.noreply.github.com>
2026-03-08 09:31:26 +0300

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8000/api'; // Update this with your actual backend URL
+const API_BASE_URL = 'https://health-system-backend-l7m5.onrender.com/api'; // Update this with your actual backend URL
 
 const ApiService = {
     // Helper for making requests
